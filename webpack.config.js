938aedb8608916e4cc7b8a74b2655649d007429e
Chethan Prabhu <chethanprabhu65@gmail.com>
2023-08-18 22:42:01 +0530

    added comments in config file

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,23 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",//telling our webpack which is the entry point of our app;
     output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "bundle.js"
+        path: path.join(__dirname, "/dist"), //path of our bundle file
+        filename: "bundle.js" // file name of our output bundle file
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: "./public/index.html"
+            template: "./public/index.html" //mention the main index  file which will be used  by react to load our application
         })
     ],
     module: {
         rules: [
             {
-                test: /.js$/,
-                exclude: /node_modules/,
+                test: /.js$/, //mention what files to look for to do the bundling
+                exclude: /node_modules/, //mention what files the bundler needs to ignore
                 use: {
-                    loader: "babel-loader",
+                    loader: "babel-loader", //mention  whhich  loader are we using for bundling
                     options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                        presets: ["@babel/preset-env", "@babel/preset-react"] // mention the presets for js and react
                     }
                 }
             }
