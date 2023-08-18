24b0cfd786e40b6e4b9f6ca563692808f1898c9c
Chethan Prabhu <chethanprabhu65@gmail.com>
2023-08-18 23:15:02 +0530

    made a sample api call from our backend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom/client";
+import axios from "axios"
 
 const App = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get("https://chethantest.netlify.app/.netlify/functions/app/getAllProducts");
+            setData(res.data);
+        })()
+    }, [])
+
     return (
-        <h1>REACT!!</h1>
+        <>
+            <h1>REACT!!</h1>
+            {data.map((item) => {
+                return <p>{item.title}</p>
+            })}
+        </>
     )
 }
 
