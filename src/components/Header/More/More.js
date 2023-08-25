ae80e09eba46b21e04d71b294e54ea6a93af6eea
Chethan Prabhu <chethanprabhu65@gmail.com>
2023-08-25 12:56:09 +0530

    Added menu items

--- src/components/Header/More/More.js
+++ src/components/Header/More/More.js
@@ -1,10 +1,25 @@
-import { Button } from "@mui/material";
-import React from "react"
+import { Button, Menu, MenuItem } from "@mui/material";
+import React, { useState } from "react"
 import "./More.css"
 
 const More = () => {
+    const [anchor, setAnchor] = useState(null);
+
+    const menuInteractedHandler = (e) => {
+        setAnchor(e.target);
+    }
+
     return (
-        <Button className="more" variant="text">More</Button>
+        <>
+            <Button id="more" onMouseEnter={menuInteractedHandler} className="more" variant="text">More</Button>
+            <Menu className="menu" open={Boolean(anchor)} onClose={() => setAnchor(null)} anchorEl={anchor}>
+                <MenuItem>My Orders</MenuItem>
+                <MenuItem>Pricing</MenuItem>
+                <MenuItem>Contact Us</MenuItem>
+                <MenuItem>FAQ</MenuItem>
+                <MenuItem>Settings</MenuItem>
+            </Menu>
+        </>
     )
 }
 
